f148f0bbe1bf76279f08faf4122a1ef54e3eb65c
ThomasKindermann <kindermann@kitconcept.com>
2022-08-12 09:48:37 +0200

    socket.io first implementation

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const port = 5001;
+
+let usersConnected = 0;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  usersConnected++;
+  console.log(
+    'a user connected',
+    socket.id.slice(-3),
+    ' - users:',
+    usersConnected
+  );
+
+  socket.on('disconnect', () => {
+    usersConnected--;
+    console.log(
+      'user disconnected',
+      socket.id.slice(-3),
+      ' - users:',
+      usersConnected
+    );
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg + ' ' + socket.id.slice(-3));
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
