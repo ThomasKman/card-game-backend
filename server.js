b99b5413a130275bad67c2a1e499c50e71676366
ThomasKindermann <kindermann@kitconcept.com>
2022-12-08 12:26:39 +0100

    add session singleton

--- server.js
+++ server.js
@@ -2,50 +2,23 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
-
 const Lobby = require('./Lobby/lobby');
-const connection = require('./Connection/connection');
 
 const port = 5001;
 
-const lobby = new Lobby(io);
-let connections = [];
+// create singleton Session
+const session = require('./Session/Session');
+
+// Start Socket Session
+session.start(server);
+
+// Create new Game Lobby
+const lobby = new Lobby();
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  addConnection(io, socket);
-  console.log('user connected,', 'users: ', connections.length);
-
-  socket.on('disconnect', () => {
-    removeConnection(socket.id);
-    console.log('user disconnected', 'users:', connections.length);
-  });
-
-  socket.on('joinRoom', () => {});
-});
-
 server.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
-
-connections.forEach((connection) => {
-  connection.connectionCheck(connection.test);
-});
-
-// connection.emitSomeShit('someShit');
-
-const addConnection = (io, socket) => {
-  connections.push(new connection(io, socket));
-};
-
-const removeConnection = (id) => {
-  const index = connections.findIndex((connection) => connection.id === id);
-  if (index !== -1) {
-    return connections.splice(index, 1)[0];
-  }
-};
